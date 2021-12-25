54f2ecff48f90470bd8b6d240954254640d21aa5
marnixah <marnix@hagehoeve.com>
2021-12-25 16:14:42 +0100

    fix(emojiSlot): remove useless wrapping view

--- components/emojiSlot.tsx
+++ components/emojiSlot.tsx
@@ -12,31 +12,22 @@ interface Props {
 export default function EmojiSlot({ disabled, slot }: Props) {
   return (
     <TouchableOpacity>
-      <View
-        style={
-          {
-            // paddingVertical: 5,
-            // paddingHorizontal: 10,
-            // backgroundColor: Colors.lighter,
-          }
-        }>
-        <Text
-          style={{
-            fontSize: 30,
-            textAlign: 'center',
-            color: '#ff0000',
-            backgroundColor: disabled ? Colors.light : Colors.white,
-            width: 50,
-            height: 50,
-            padding: 5,
-            margin: 5,
-          }}>
-          {
-            //@ts-ignore - It is correct... I-.. I think
-            emojiMap[slot.value]
-          }
-        </Text>
-      </View>
+      <Text
+        style={{
+          fontSize: 30,
+          textAlign: 'center',
+          color: '#ff0000',
+          backgroundColor: disabled ? Colors.light : Colors.white,
+          width: 50,
+          height: 50,
+          padding: 5,
+          margin: 5,
+        }}>
+        {
+          //@ts-ignore - It is correct... I-.. I think
+          emojiMap[slot.value]
+        }
+      </Text>
     </TouchableOpacity>
   );
 }
